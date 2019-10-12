37f0fc8083db5ed1f453bfe56e4be9dd0aff1b28
mikuspikus <pyro74rus@gmail.com>
2019-10-12 21:59:57 +0300

    Added custom auth token generation

--- user/api/views.py
+++ user/api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.views import APIView, Request, Response
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
 from api.serializers import CustomUserSerializer
 from api.models import CustomUser
 
@@ -12,6 +15,15 @@ from logging import Logger
 class BaseView(APIView):
     logger = Logger(name = 'user.api.views')
 
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data = request.data, context = {'request': request})
+        serializer.is_valid(raise_exception = True)
+
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user = user)
+
+        return Response({'token': token.key, 'user_id': user.pk, 'uuid' : user.outer_uuid}, status = status.HTTP_200_OK)
 
 class UserInfoView(BaseView):
 
@@ -77,4 +89,6 @@ class RegisterView(BaseView):
         if serializer.is_valid():
             serializer.save()
 
-            return Response(data = serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(data = serializer.data, status = status.HTTP_201_CREATED)
+
+        return Response(data = serializer.errors, status = status.HTTP_400_BAD_REQUEST)
