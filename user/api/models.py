051f4a1c73a26f8122f69135892335d64682494e
mikuspikus <pyro74rus@gmail.com>
2019-10-12 22:00:43 +0300

    Commented custom user model realization

--- user/api/models.py
+++ user/api/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 import uuid
 
 # created my models here
+'''
+class CustomUserManager(BaseUserManager):
+    use_in_migration = True
+
+    def create_user(self, username: str, password: str):
+        user = self.model(
+            username = username
+        )
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+
+    def create_staffuser(self, username: str, password: str):
+        user = self.create_user(
+            username = username,
+            password = password
+        )
+
+        user.staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username: str,  password: str):
+        user = self.create_user(
+            username = username,
+            password = password
+        )
+        user.staff = True
+        user.admin = True
+        
+        user.save(using=self._db)
+
+        return user
+'''
+
 class CustomUser(AbstractUser):
     outer_uuid = models.UUIDField(unique = True, default = uuid.uuid4, editable = False)
+
+    #objects = CustomUserManager()
